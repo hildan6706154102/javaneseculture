7f8e492ed99c26a075ab188af7bf49456a284572
ferdiansyah6706154192 <edosetiawan108@gmail.com>
2017-04-21 19:23:52 +0700

    Add files via upload

---
+++ javaneseculture/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Input Mahasiswa-2062</title>
+	<link rel="stylesheet" type="text/css" href="css/sidebar.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+</head>
+<body>
+	<div id="container">
+		<ul class="nav nav-pills nav-stacked sidebar">
+			 <li role="presentation" class="active"><a href="index.php">Home</a></li>
+			 <li role="presentation"><a href="provinsi.php">Provinsi</a></li>
+			 <li role="presentation"><a href="view.php">View Culture</a></li>
+			 <li role="presentation"><a href="search.php">Search Culture</a></li>
+			 <li role="presentation"><a href="update.php">Update Culture</a></li>
+		</ul>
+		<div id="content">
+			<form class="form-horizontal" method="post">
+				<div class="form-group">
+				    
+				    <div class="col-sm-10">
+				     
+				    </div>
+				</div>
+				<div class="form-group">
+				    
+				    <div class="col-sm-10">
+				      
+				    </div>
+				</div>
+				<div class="form-group">
+				    <div class="col-sm-offset-2 col-sm-10">
+				      
+					</div>
+  				</div>
+			</form>
+			
+		</div>
+	</div>
+</body>
+</html>
